509b8ea855695b267ab36f4bb95d9e7566bfddb5
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-12-08 09:54:39 +0600

    Authentication system added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <Header />
-      <body className="font-poppins">{children}</body>
-      <Footer />
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className="font-poppins">
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,14 @@ import MobileMenu from "./MobileMenu";
 import SearchBar from "./SearchBar";
 import CartIcon from "./CartIcon";
 import FavouriteButton from "./FavouriteButton";
+import { currentUser } from "@clerk/nextjs/server";
+import { ClerkLoaded, SignedIn, UserButton } from "@clerk/nextjs";
+import SignIn from "./SignIn";
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser();
+  console.log("user", user);
+
   return (
     <header className="sticky top-0 z-50 py-5 bg-white/70 backdrop-blur-md">
       <Container className="flex items-center justify-between text-lightColor">
@@ -20,6 +26,12 @@ const Header = () => {
           <SearchBar />
           <CartIcon />
           <FavouriteButton />
+          <ClerkLoaded>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {!user && <SignIn />}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
