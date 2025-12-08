80a9ce83746ffa9b7b5e2b78bed35ba450ad218a
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-12-08 18:50:40 +0600

    Deal products page created

--- sanity/lib/queries/index.ts
+++ sanity/lib/queries/index.ts
@@ -1,5 +1,5 @@
 import { sanityFetch } from "../live";
-import { BRANDS_QUERY, LATEST_BLOG_QUERY } from "./query";
+import { BRANDS_QUERY, DEAL_PRODUCTS, LATEST_BLOG_QUERY } from "./query";
 
 const getCategories = async (quantity?: number) => {
   try {
@@ -43,4 +43,14 @@ const getLatestBlogs = async () => {
   }
 };
 
-export { getCategories, getAllBrands, getLatestBlogs };
+const getDealProducts = async () => {
+  try {
+    const { data } = await sanityFetch({ query: DEAL_PRODUCTS });
+    return data ?? [];
+  } catch (error) {
+    console.log("Error fetching deal Products:", error);
+    return [];
+  }
+};
+
+export { getCategories, getAllBrands, getLatestBlogs, getDealProducts };
